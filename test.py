ea3daacfda6e0ce392bbc5c05716a4d58b8933f0
Jinho Lee <jnonyx@gmail.com>
2021-03-23 14:23:34 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-import cgi
-form = cgi.FieldStorage()
-sdate_form =  form.getvalue('sdate')
+#!C:\Users\jnony\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.9\python.exe
+import cgi, os
 
-print("Start Date was " + sdate_form)
+print('content-type:text/html\r\n\r\n')
+
+form = cgi.FieldStorage()
+pn = str(form.getvalue("pname"))
+des = str(form.getvalue("des"))
+fle = form['filename']
+
+fn = os.path.basename(fle.filename)
+open("https://github.com/jnonyx/html_py/" +fn, "wb").write(fle.file.read())
+     
+print('<html>')
+print('<body>')
+print('<h1>Product Name\n(%s)</h1>'%pn)
+print('<h2>%s</h2>'%des)
+
+    
